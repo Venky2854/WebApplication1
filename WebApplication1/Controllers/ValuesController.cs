c22ec109c0082686dbde2f8a87c8b532f221b769
Venkatesh <Venkatesh@VENKATESH>
2022-05-18 12:44:21 +0530

    Add project files.

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "car", "bus", "Aeroplane", "bike" };
+        }
+
+        [HttpGet("{id}")]
+
+        public string Get(int id)
+        {
+            return "The value is" + id;
+        }
+    }
+}
